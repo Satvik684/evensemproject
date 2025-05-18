db9d557b71a8c263760be9ff6300b650c1858488
Amanihh <amankumar9911675026@gmail.com>
2025-05-18 20:11:14 +0530

    frontend added 1st time

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './assets/Navbar'
+import Midsec from './assets/Midsec'
+import Scholarship from './assets/Scholarship'
+
+
+const App = () => {
+  return (
+    <>
+  <Navbar />
+  <Midsec />
+  <Scholarship />
+</>
+  )
+}
+
+export default App
---
+++ frontend/src/assets/Midsec.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const midsec = () => {
+  return (
+    <div>
+        <section className="bg-gray-100 py-16 px-4 text-center">
+  <h1 className="py-2 text-5xl md:text-5xl font-extrabold text-gray-800 mb-10">
+    BROWSE SCHOLARSHIPS <br className="hidden md:block" /> FOR COLLEGE STUDENTS
+  </h1>
+  <p className="text-gray-500 max-w-2xl mx-auto mb-8 text-lg">
+    The nation’s largest, multi-billion dollar scholarship database — create a free account to see all of your personalized matches and start applying today.
+  </p>
+  <button className="bg-blue-600 hover:bg-blue-700 text-white font-medium px-6 py-2 rounded shadow">
+    Get Started
+  </button>
+</section>
+    </div>
+  )
+}
+
+export default midsec
