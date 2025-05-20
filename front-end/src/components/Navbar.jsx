e1a585261deea7ec41e6bb2f834ee88b776c2de8
Satvik Singh <satvik121singh@gmail.com>
2025-05-20 20:20:53 +0530

    home link added

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 // src/components/Navbar.jsx
 import React, { useState } from 'react';
 import LoginModal from './LoginModal';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [isModalOpen, setModalOpen] = useState(false);
@@ -11,7 +12,7 @@ const Navbar = () => {
         <h2 className="text-2xl font-bold">Aman</h2>
         <div className="flex items-center gap-10">
           <h2 className="text-xl hover:text-blue-400 cursor-pointer">List</h2>
-          <h2 className="text-xl hover:text-blue-400 cursor-pointer">Home</h2>
+          <Link to={'/'}><h2 className="text-xl hover:text-blue-400 cursor-pointer">Home</h2></Link>
           <h2 className="text-xl hover:text-blue-400 cursor-pointer">About</h2>
           <button onClick={() => setModalOpen(true)} className="text-xl hover:text-blue-400 cursor-pointer">
             Login
