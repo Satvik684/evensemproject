86f77df3c5226c727cf71ff932eb0cb0ffd25220
Satvik Singh <satvik121singh@gmail.com>
2025-05-29 18:51:27 +0530

    final

--- front-end/src/hooks/useLogout.jsx
+++ front-end/src/hooks/useLogout.jsx
@@ -1,14 +1,17 @@
+import {  useNavigate } from "react-router-dom";
 import { useAuthContext } from "./useAuthContext";
 
 
 export const useLogout = () => {
     const { dispatch } = useAuthContext();
-
+    const navigate = useNavigate();
     const logout = () =>{
         //remove user from storage
         localStorage.removeItem('user');
 
         dispatch({type:'LOGOUT'});
+
+        navigate('/');
     }
 
     return {logout};
